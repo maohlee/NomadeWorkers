63bf488db089a386f2c23f71d841ded05dcb1ab8
unknown <Administrateur@FORMATION.LOCAL>
2016-04-19 11:00:56 +0200

    Interface de base

---
+++ app/src/main/java/com/example/mao/nomadeworkers/activities/ClientShowActivity.java
@@ -0,0 +1,17 @@
+package com.example.mao.nomadeworkers.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.mao.nomadeworkers.R;
+
+public class ClientShowActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client_show);
+    }
+}
